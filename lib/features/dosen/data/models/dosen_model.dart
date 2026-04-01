350c57292681207246d3137a47e30a1cb17b49ad
Abdullah Azzam <abdazzm24@gmail.com>
2026-04-01 23:16:08 +0700

    modul 6

--- lib/features/dosen/data/models/dosen_model.dart
+++ lib/features/dosen/data/models/dosen_model.dart
@@ -1,35 +1,4 @@
-class DosenModel {
-  final String name;
-  final String username;
-  final String email;
-  final AddressModel address;
-
-  DosenModel({
-    required this.name,
-    required this.username,
-    required this.email,
-    required this.address,
-  });
-
-  factory DosenModel.fromJson(Map<String, dynamic> json) {
-    return DosenModel(
-      name: json['name'] ?? '',
-      username: json['username'] ?? '',
-      email: json['email'] ?? '',
-      address: AddressModel.fromJson(json['address'] ?? {}),
-    );  
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'name': name,
-      'username': username,
-      'email': email,
-      'address': address.toJson(),
-    };
-  }
-}
-
+/// Model untuk data alamat dosen (nested object dari DosenModel)
 class AddressModel {
   final String street;
   final String suite;
@@ -45,10 +14,10 @@ class AddressModel {
 
   factory AddressModel.fromJson(Map<String, dynamic> json) {
     return AddressModel(
-      street: json['street'],
-      suite: json['suite'],
-      city: json['city'],
-      zipcode: json['zipcode'],
+      street: json['street'] ?? '',
+      suite: json['suite'] ?? '',
+      city: json['city'] ?? '',
+      zipcode: json['zipcode'] ?? '',
     );
   }
 
@@ -58,6 +27,42 @@ class AddressModel {
       'suite': suite,
       'city': city,
       'zipcode': zipcode,
+    };
+  }
+}
+
+class DosenModel {
+  final int id;
+  final String name;
+  final String username;
+  final String email;
+  final AddressModel address;
+
+  DosenModel({
+    required this.id,
+    required this.name,
+    required this.username,
+    required this.email,
+    required this.address,
+  });
+
+  factory DosenModel.fromJson(Map<String, dynamic> json) {
+    return DosenModel(
+      id: json['id'] ?? 0,
+      name: json['name'] ?? '',
+      username: json['username'] ?? '',
+      email: json['email'] ?? '',
+      address: AddressModel.fromJson(json['address'] ?? {}),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'username': username,
+      'email': email,
+      'address': address.toJson(),
     };
   }
 }
--- lib/features/dosen/data/repositories/dosen_repositories.dart
+++ lib/features/dosen/data/repositories/dosen_repositories.dart
@@ -1,19 +1,23 @@
 import 'package:dio/dio.dart';
 import 'package:week4/features/dosen/data/models/dosen_model.dart';
+import 'package:week4/core/network/dio_client.dart';
 
 class DosenRepositories {
-  final Dio _dio = Dio();
+  final DioClient _dioClient;
 
+  DosenRepositories({DioClient? dioClient})
+    : _dioClient = dioClient ?? DioClient();
+
+  // get data daftar dosen
   Future<List<DosenModel>> getDosenList() async {
     try {
-      final response = await _dio.get(
-        'https://jsonplaceholder.typicode.com/users',
-      );
-
-      final List data = response.data;
+      final Response response = await _dioClient.dio.get('/users');
+      final List<dynamic> data = response.data;
       return data.map((json) => DosenModel.fromJson(json)).toList();
-    } catch (e) {
-      throw Exception('Gagal memuat data dosen: $e');
+    } on DioException catch (e) {
+      throw Exception(
+        'Gagal memuat data dosen: ${e.response?.statusCode} - ${e.message}',
+      );
     }
   }
 }
